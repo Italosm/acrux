4225ffb2b2c2fd0b7a0b510a7d711fcc8e4ee737
Italo Souza Melo <italosm29@gmail.com>
2023-07-10 22:50:03 -0300

    container tsyringe att

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,11 +1,26 @@
 import { container } from 'tsyringe';
+import '@modules/users/providers';
 
 import { IUsersRepository } from '@modules/users/domain/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
-import '@modules/users/providers';
+import { IRolesRepository } from '@modules/users/domain/repositories/IRolesRepository';
+import RolesRepository from '@modules/users/infra/typeorm/repositories/RolesRepository';
+
+import { IPermissionsRepository } from '@modules/users/domain/repositories/IPermissionsRepository';
+import PermissionsRepository from '@modules/users/infra/typeorm/repositories/PermissionsRepository';
 
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
+);
+
+container.registerSingleton<IRolesRepository>(
+  'RolesRepository',
+  RolesRepository,
+);
+
+container.registerSingleton<IPermissionsRepository>(
+  'PermissionsRepository',
+  PermissionsRepository,
 );
